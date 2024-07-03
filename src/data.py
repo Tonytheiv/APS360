39b8b7d12d4d9bd7f3a71a78d4f79aa06212fdc0
tony <tonytheiv@icloud.com>
2024-07-02 23:28:02 -0400

    added dataloader

--- src/data.py
+++ src/data.py
@@ -1,68 +1,125 @@
+##########################################################################################################
+'''
+Author: Tony Wang
+'''
+##########################################################################################################
+
 import os
-import torch
-from torch.utils.data import Dataset
+import cv2
 import pandas as pd
 import numpy as np
-from scipy import interpolate
+import torch
+from torch.utils.data import Dataset, DataLoader
+from tqdm import tqdm
 
-class UCYDataset(Dataset):
-    def __init__(self, csv_dir, transform=None):
-        self.csv_dir = csv_dir
-        self.csv_files = [f for f in os.listdir(csv_dir) if f.endswith('.csv')]
+class PedestrianTrackingDataset(Dataset):
+    def __init__(self, video_dir, label_dir, image_dir, transform=None):
+        """
+        Initializes the dataset with directories for videos, labels, and images.
+
+        Args:
+            video_dir (str): Directory containing video files.
+            label_dir (str): Directory containing label CSV files.
+            image_dir (str): Directory to store extracted images.
+            transform (callable, optional): A function/transform to apply to the images.
+        """
+        self.video_dir = video_dir
+        self.label_dir = label_dir
+        self.image_dir = image_dir
         self.transform = transform
+        self.video_files = [f for f in os.listdir(video_dir) if f[0].isdigit() and f.endswith('.avi')]
+        self.video_files.sort()
         
+        self._prepare_images()
+        
+    def _prepare_images(self):
+        """
+        Extracts frames from video files and saves them as JPEG images in the image directory.
+        This function is only run if the images do not already exist.
+        """
+        if not os.path.exists(self.image_dir):
+            os.makedirs(self.image_dir)
+        
+        for video_file in tqdm(self.video_files, desc='Extracting frames'):
+            video_path = os.path.join(self.video_dir, video_file)
+            video_id = video_file.split('.')[0]
+
+            cap = cv2.VideoCapture(video_path)
+            frame_id = 0
+            while cap.isOpened():
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                
+                image_name = f'{video_id}_frame{frame_id}.jpg'
+                image_path = os.path.join(self.image_dir, image_name)
+                
+                if not os.path.exists(image_path):
+                    cv2.imwrite(image_path, frame)
+                
+                frame_id += 1
+            cap.release()
+
     def __len__(self):
-        return len(self.csv_files)
+        """
+        Returns the total number of images in the dataset.
+        """
+        return len([f for f in os.listdir(self.image_dir) if f.endswith('.jpg')])
     
     def __getitem__(self, idx):
-        csv_path = os.path.join(self.csv_dir, self.csv_files[idx])
-        data = pd.read_csv(csv_path, delimiter=',', header=None).values
-        interpolated_data = self.interpolate_missing(data)
-        filtered_data = self.filter_frames(interpolated_data)
-        
-        sample = {'data': filtered_data}
-        
-        if self.transform:
-            sample = self.transform(sample)
-        
-        return sample
-    
-    def interpolate_missing(self, data):
-        interpolated_data = np.empty((0, 4))
-        
-        max_id = int(np.max(data[:, 1]) + 1)
-        for i in range(max_id):
-            traj_of_ped_i = data[data[:, 1] == i, :]
-            if traj_of_ped_i.size == 0:
-                continue
-            y = int(traj_of_ped_i[-1, 0])
-            x = int(traj_of_ped_i[0, 0])
-            
-            while x <= y:
-                if x in traj_of_ped_i[:, 0]:
-                    interpolated_data = np.append(interpolated_data, traj_of_ped_i[traj_of_ped_i[:, 0] == x], axis=0)
-                    x += 1
-                else:
-                    f = interpolate.interp1d(traj_of_ped_i[:, 0], [traj_of_ped_i[:, 2], traj_of_ped_i[:, 3]], 
-                                             fill_value="extrapolate", bounds_error=False)
-                    inter = f(x)
-                    interpolated_data = np.append(interpolated_data, np.array([[int(x), int(i), float(inter[0]), float(inter[1])]]), axis=0)
-                    x += 1
-                if x == y + 1:
-                    break
-        interpolated_data = interpolated_data[np.argsort(interpolated_data[:, 0])]
-        return interpolated_data
-    
-    def filter_frames(self, data, frame_interval=10):
-        frames = sorted(np.unique(data[:, 0]))
-        final_data = []
-        for frame in frames:
-            if frame % 10 == 0 or frame == 0:
-                final_data.extend(data[data[:, 0] == frame, :])
-        final_data = np.unique(final_data, axis=0)
-        return np.array(final_data)
+        """
+        Retrieves an image and its corresponding labels.
 
-class ToTensor(object):
-    def __call__(self, sample):
-        data = sample['data']
-        return {'data': torch.from_numpy(data).float()}
+        Args:
+            idx (int): Index of the image to retrieve.
+        
+        Returns:
+            image (Tensor): The image tensor.
+            labels (Tensor): Corresponding labels for the image.
+        """
+        image_files = [f for f in os.listdir(self.image_dir) if f.endswith('.jpg')]
+        image_files.sort()
+        image_file = image_files[idx]
+        image_path = os.path.join(self.image_dir, image_file)
+        video_id = image_file.split('_frame')[0]
+        frame_id = int(image_file.split('_frame')[1].replace('.jpg', ''))
+        
+        image = cv2.imread(image_path)
+        if self.transform:
+            image = self.transform(image)
+        
+        label_file = os.path.join(self.label_dir, f'{video_id}.csv')
+        labels_df = pd.read_csv(label_file, header=None)
+        labels_df.columns = ['FrameID', 'PedestrianID', 'PosX', 'PosY']
+        
+        labels = labels_df[labels_df['FrameID'] == frame_id][['PedestrianID', 'PosX', 'PosY']].values
+        return torch.tensor(image, dtype=torch.float32).permute(2, 0, 1), torch.tensor(labels, dtype=torch.float32)
+
+def collate_fn(batch):
+    """
+    Custom collate function to handle batching of images and labels.
+
+    Args:
+        batch (list): List of tuples containing images and labels.
+    
+    Returns:
+        images (Tensor): Batched images.
+        labels (list): Batched labels.
+    """
+    images, labels = zip(*batch)
+    images = torch.stack(images, dim=0)
+    return images, labels
+
+if __name__ == "__main__":
+    video_dir = '/home/tony/APS360/baseline/UCY_dataset'
+    label_dir = '/home/tony/APS360/baseline/processed_data'
+    image_dir = '/home/tony/APS360/src/datasets/ucy'
+    
+    dataset = PedestrianTrackingDataset(video_dir, label_dir, image_dir)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True, collate_fn=collate_fn)
+
+    for batch_idx, (images, labels) in enumerate(dataloader):
+        print(f'Batch {batch_idx}:')
+        print('Images shape:', images.shape)
+        print('Labels:', labels)
+        break
